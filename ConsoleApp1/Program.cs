93ef321cec9a3213b1f42a2b9d5009ffdd676a89
Musodzhon Zakirov <i.zulayho@gmail.com>
2021-08-20 22:43:00 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Rectangle
+        {
+            public double side1, side2;
+
+            public Rectangle(double side1, double side2)
+            {
+                this.side1 = side1;
+                this.side2 = side2;
+            }
+
+            public double AreaCalculator()
+            {
+                return side1 * side2;
+            }
+            public double PerimeterCalculator()
+            {
+                return 2 * (side1 + side2);
+            }
+            public double Area
+            {
+                get { return AreaCalculator(); }
+            }
+
+            public double Perimeter
+            {
+                get { return PerimeterCalculator(); }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(10, 20);
+            Console.WriteLine($" площадь: {rectangle.Area}, периметр: {rectangle.Perimeter}");
+
+            Rectangle rectangle1 = new Rectangle(12.80, 16.60);
+            Console.WriteLine($" площадь: {Math.Round(rectangle1.Area,2)}, периметр: {Math.Round(rectangle1.Perimeter,2)}");
+        }
+    }
+}
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Book
+    {
+        public Title title { get; set; }
+        public Author author { get; set; }
+        public Content content { get; set; }
+
+        public Book(string titleId, string authorId, string contentId)
+        {
+            title = new Title();
+            author = new Author();
+            content = new Content();
+            title.title = titleId;
+            author.author = authorId;
+            content.content = contentId;
+
+        }
+        public void Show()
+        {
+            title.Show();
+            author.Show();
+            content.Show();
+            
+        }
+    }
+    class Title
+    {
+        public string title = "";
+        public void Show()
+        {
+            Console.Write("Название: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+    }
+    class Author
+    {
+        public string author = "";
+        public void Show()
+        {
+            Console.Write("Автор: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(author);
+            Console.ResetColor();
+        }
+    }
+    class Content
+    {
+        public string content = "";
+        public void Show()
+        {
+            Console.Write("контент: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(content);
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("C# 4.0: полное руководство", "Герберт Шилдт", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis.\n");
+            book.Show();
+
+            Book book1 = new Book("НИ СЫ", "Джен Синсеро", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis.\n");
+            book1.Show();
+
+            Book book2 = new Book("Записки Сумасшедшего", "Лу Синь", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis.\n");
+            book2.Show();
+
+        }
+    }
+}
